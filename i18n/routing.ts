dd41b068b19a5a95ead26c2c8503802b49d0132d
Doan Hai Duy <haiduytbt2k3@gmail.com>
2026-08-22 20:32:09 +0700

    fix(i18n): force default locale to /vi and disable browser auto-language detection

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,8 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["vi", "en"],
   defaultLocale: "vi",
+  localeDetection: false,
+  localePrefix: "always",
 });
 
 export type Locale = (typeof routing.locales)[number];
